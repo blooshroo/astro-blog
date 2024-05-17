f39bc70ff81fa84062df954768a50e48fe89e05b
blooshroo <serinadok@gmail.com>
2024-05-17 12:38:48 +0200

    add features & complete tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://master--the-training-of-the-shrew.netlify.app/",
+  integrations: [preact()]
+});
